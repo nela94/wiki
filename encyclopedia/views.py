56174a8d5185940918c42d32490c4cd3409b9657
Manuela Sanchez <m.sanchez947@Hotmail.com>
2021-01-07 20:15:13 -0500

    can show entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -8,3 +9,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def get_title(request, title):
+    data = util.get_entry(title)
+    return render(request, "encyclopedia/entry.html",{
+        "body_info": data,
+        "title": title
+    })
